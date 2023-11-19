acb1bd5aa6dcad174700aff21f51c2d41f486eff
Berk Durmus <berkdurmus@yahoo.com>
2023-11-19 09:52:34 +0300

    feat(protocol) : implement comm protocol

--- communication_protocol.go
+++ communication_protocol.go
@@ -1,12 +1,69 @@
+package communication
 
-        package communication
+import (
+    "fmt"
+    "net"
+)
 
-        // Communication protocol for node-to-node interactions
-        type Protocol struct {
-            // Protocol properties and methods
+// Protocol struct represents the communication protocol for node-to-node interactions
+type Protocol struct {
+    // Add relevant properties here, like connection details
+    Address string // Address of the node
+}
+
+// NewProtocol creates a new Protocol instance
+func NewProtocol(address string) *Protocol {
+    return &Protocol{
+        Address: address,
+    }
+}
+
+// SendMessage sends a message to a specified address
+func (p *Protocol) SendMessage(addr string, message string) error {
+    conn, err := net.Dial("tcp", addr)
+    if err != nil {
+        return fmt.Errorf("error connecting to node: %v", err)
+    }
+    defer conn.Close()
+
+    _, err = conn.Write([]byte(message))
+    if err != nil {
+        return fmt.Errorf("error sending message: %v", err)
+    }
+
+    return nil
+}
+
+// ReceiveMessage listens for incoming messages
+func (p *Protocol) ReceiveMessage() {
+    ln, err := net.Listen("tcp", p.Address)
+    if err != nil {
+        fmt.Printf("error setting up listener: %v\n", err)
+        return
+    }
+
+    for {
+        conn, err := ln.Accept()
+        if err != nil {
+            fmt.Printf("error accepting connection: %v\n", err)
+            continue
         }
 
-        func NewProtocol() *Protocol {
-            return &Protocol{}
-        }
-    
+        go p.handleConnection(conn)
+    }
+}
+
+// handleConnection handles an individual connection from a node
+func (p *Protocol) handleConnection(conn net.Conn) {
+    defer conn.Close()
+
+    buffer := make([]byte, 1024)
+    len, err := conn.Read(buffer)
+    if err != nil {
+        fmt.Printf("error reading message: %v\n", err)
+        return
+    }
+
+    fmt.Printf("Received message: %s\n", string(buffer[:len]))
+}
+
