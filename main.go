678e86fa63f800e7d0dab1da9a172a6c6a9d483d
Berk Durmus <berkdurmus@yahoo.com>
2023-11-19 09:59:41 +0300

    feat(main): cover all dist functionalities

--- main.go
+++ main.go
@@ -1,17 +1,58 @@
+package main
 
-        package main
+import (
+    "fmt"
+    "os"
+    "github.com/user/godistsys/node"
+    "github.com/user/godistsys/scheduler"
+    "github.com/user/godistsys/communication"
+    "github.com/user/godistsys/utils"
+)
 
-        import (
-            "fmt"
-            "log"
-            "net/http"
+func main() {
+    // Initialize a logger
+    logger := utils.NewLogger("GoDistSys")
 
-            "github.com/user/godistsys/node"
-            "github.com/user/godistsys/scheduler"
-            // Other imports
-        )
+    // Create a scheduler
+    sch := scheduler.NewScheduler()
 
-        func main() {
-            // Initialization and startup logic for the distributed system
+    // Add some nodes to the scheduler
+    for i := 0; i < 5; i++ {
+        n := node.NewNode(fmt.Sprintf("node-%d", i))
+        sch.AddNode(n)
+    }
+
+    // Initialize the communication protocol (assuming it runs on its own goroutine or similar)
+    protocol := communication.NewProtocol("localhost:8080")
+    go protocol.ReceiveMessage()
+
+    // Schedule some tasks
+    for i := 0; i < 10; i++ {
+        task := &scheduler.Task{
+            ID:      fmt.Sprintf("task-%d", i),
+            Payload: "Sample Task Payload",
         }
-    
+        sch.ScheduleTask(task)
+    }
+
+    // Example of sending a message (to the first node for simplicity)
+    if len(sch.Nodes) > 0 {
+        err := protocol.SendMessage(sch.Nodes[0].ID, "Hello Node")
+        if err != nil {
+            logger.Error(err)
+        } else {
+            logger.Info("Message sent successfully")
+        }
+    } else {
+        logger.Error(fmt.Errorf("no nodes available for messaging"))
+    }
+
+    // Process tasks on each node
+    for _, n := range sch.Nodes {
+        n.ProcessTasks()
+    }
+
+    // Exiting the application
+    logger.Info("Exiting GoDistSys")
+    os.Exit(0)
+}
